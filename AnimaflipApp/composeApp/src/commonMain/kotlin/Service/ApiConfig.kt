6accc768a6eaef56c0928491969d2407d6d1231c
dgoult <dylan.goult@limayrac.fr>
2024-09-02 09:22:21 +0200

    Génération de l'apk signé api sur http://192.168.1.155

--- AnimaflipApp/composeApp/src/commonMain/kotlin/Service/ApiConfig.kt
+++ AnimaflipApp/composeApp/src/commonMain/kotlin/Service/ApiConfig.kt
@@ -1,6 +1,6 @@
 package Service
 
 object ApiConfig {
-    // Cette adresse correspond à l'accès local depuis l'emulateur android studio
-    const val BASE_URL = "http://10.0.2.2" //Faire correspondre au domaine du serveur Ex : const val BASE_URL = "http://192.168.1.155"
+    // Cette adresse http://10.0.2.2 correspond à l'accès local depuis l'emulateur android studio
+    const val BASE_URL = "http://192.168.1.155" //Faire correspondre au domaine du serveur Ex : const val BASE_URL = "http://192.168.1.155"
 }
