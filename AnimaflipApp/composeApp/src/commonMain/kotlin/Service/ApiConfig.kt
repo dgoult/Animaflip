3b868ee7e45053b2ed771621fb7051094650fbe2
dgoult <dylan.goult@limayrac.fr>
2024-09-02 02:31:52 +0200

    Modification BASE_URL

--- AnimaflipApp/composeApp/src/commonMain/kotlin/Service/ApiConfig.kt
+++ AnimaflipApp/composeApp/src/commonMain/kotlin/Service/ApiConfig.kt
@@ -1,5 +1,5 @@
 package Service
 
 object ApiConfig {
-    const val BASE_URL = "http://10.0.2.2"
+    const val BASE_URL = "http://10.0.2.2" //Faire correspondre au domaine du serveur Ex : const val BASE_URL = "http://192.168.1.155"
 }
